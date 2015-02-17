0442dbaadd9e44058e7552f7aede5bd958c3575b
SpockReko <stefan.fritzon@gmail.com>
2015-02-17 13:18:53 +0100

    added files

---
+++ src/main/java/edu/gu/hajo/chat/server/spec/IChatClient.java
@@ -0,0 +1,18 @@
+package edu.gu.hajo.chat.server.spec;
+
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import edu.gu.hajo.chat.server.core.User;
+
+/**
+ * Contract for client (from Server view). Call backs from server
+ *
+ * @author hajo
+ *
+ */
+public interface IChatClient extends Remote {
+
+	public User login() throws RemoteException, NotBoundException;
+}
---
+++ src/main/java/edu/gu/hajo/chat/server/spec/IPeer.java
@@ -0,0 +1,16 @@
+package edu.gu.hajo.chat.server.spec;
+
+import java.rmi.Remote;
+
+/**
+ * One clients view of another (peer2peer)
+ *
+ * @author hajo
+ *
+ */
+public interface IPeer extends Remote {
+	
+	public void Download();
+	
+	public void cancel();
+}
